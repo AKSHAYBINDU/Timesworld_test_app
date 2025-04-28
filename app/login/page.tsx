5a6ffa7d4e32a7f18b3141c701376efa00252e77
akshaykumar <akshay.kumar@timesworld.com>
2025-04-28 17:33:04 +0530

    Folder structure created.

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function Page() {
+    return (
+        <Container>
+            <h1>Login page.</h1>
+        </Container>
+    );
+}
